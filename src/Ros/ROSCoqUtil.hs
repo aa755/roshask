e92001ec2b7d7005fc17c8c5935e55c75597a592
Abhishek Anand (@ Ubuntu15 VM on VAIO laptop) <abhishek.anand.iitg@gmail.com>
2015-09-10 00:21:28 -0400

    function to merge colists as soon as items are available in either input. copied it from the comments branch. https://github.com/aa755/roshask/commit/822d6d603a664cc94dada5a064d6b8f2c4c07979

--- src/Ros/ROSCoqUtil.hs
+++ src/Ros/ROSCoqUtil.hs
@@ -4,16 +4,26 @@
 -- via extraction.
 
 
-module Ros.ROSCoqUtil (nbind, nreturn, toListN, publishCoList , subscribeCoList) where
+module Ros.ROSCoqUtil (nbind, nreturn, toListN, publishCoList , subscribeCoList, asapMergeCoList) where
 import Ros.Topic.Util (fromList, toList)
 import Ros.Node
 import Ros.Internal.RosBinary (RosBinary)
 import Ros.Internal.Msg.MsgInfo
 import Data.Typeable.Internal
+import Control.Concurrent
 
 nreturn::a  -> Node a
 nreturn x = return x
 
+asapMerge :: [a] ->  [a] -> IO [a]
+asapMerge t1 t2 =  do
+                         c <- newChan
+                         _ <- forkIO $ writeList2Chan c t1
+                         _ <- forkIO $ writeList2Chan c t2
+                         getChanContents c
+
+asapMergeCoList :: [a] ->  [a] -> Node [a]
+asapMergeCoList t1 t2 =  liftIO (asapMerge t1 t2)
 
 toListN :: Topic IO a -> Node [a]
 toListN t = liftIO (toList t)
